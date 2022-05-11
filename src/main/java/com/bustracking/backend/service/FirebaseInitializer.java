fd37768d64111dab38d853531ba0ee6d3b7b2162
Sahan Perera <sahanyuganthamail@gmail.com>
2022-05-11 20:54:28 +0530

    some new--bn

--- src/main/java/com/bustracking/backend/service/FirebaseInitializer.java
+++ src/main/java/com/bustracking/backend/service/FirebaseInitializer.java
@@ -4,6 +4,7 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
@@ -11,8 +12,11 @@ public class FirebaseInitializer {
     public void initDB() {
 
         try {
+            FileInputStream serviceAccount =
+                    new FileInputStream("./serviceAccountKey.json");
 
-            InputStream serviceAccount = this.getClass().getClassLoader().getResourceAsStream("./serviceAccountKey.json");
+
+            //InputStream serviceAccount = this.getClass().getClassLoader().getResourceAsStream("./serviceAccountKey.json");
 
             FirebaseOptions options = new FirebaseOptions.Builder()
                     .setCredentials(GoogleCredentials.fromStream(serviceAccount))
